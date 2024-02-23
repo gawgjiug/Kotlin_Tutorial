ef20df3e3230e38011eb20cce81e3b3753a7f4cc
gwagjiug <kwjo0228@naver.com>
2024-02-23 18:50:54 +0900

    2024-02-23
    SimpleAdapterActivity 예제 코드 작성을 위한 binding 및 Activity 전환 intent 추가

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.tutorial
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         val listView = findViewById<ListView>(R.id.main_list)
         val data = resources.getStringArray(R.array.job)
@@ -18,5 +22,10 @@ class MainActivity : AppCompatActivity() {
             data
         )
         listView.adapter = adapter
+
+
+        val intent = Intent(this,SimpleAdapterActivity::class.java)
+        binding.btnSimple.setOnClickListener { startActivity(intent) }
+
     }
 }
