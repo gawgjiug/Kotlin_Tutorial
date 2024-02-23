d7a0c1fa7617c9d0d46f998da9f8ff593faec656
gwagjiug <kwjo0228@naver.com>
2024-02-23 19:22:39 +0900

    2024-02-23

---
+++ app/src/main/java/com/example/tutorial/ListView_Add_DeleteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListView_Add_DeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view_add_delete)
+    }
+}
--- app/src/main/java/com/example/tutorial/SimpleAdapterActivity.kt
+++ app/src/main/java/com/example/tutorial/SimpleAdapterActivity.kt
@@ -1,16 +1,20 @@
 package com.example.tutorial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
 import android.widget.SimpleAdapter
+import com.example.tutorial.databinding.ActivitySimpleAdapterBinding
 
 class SimpleAdapterActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivitySimpleAdapterBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_simple_adapter)
+        setContentView(binding.root)
 
-        val listView = findViewById<ListView>(R.id.simple_Listview)
+        val listView = findViewById<ListView>(R.id.simple_ListView)
         val data: ArrayList<HashMap<String,String>> = ArrayList()
         var map : HashMap<String,String> = HashMap()
         map["제품명"] = "신라면"
@@ -35,5 +39,9 @@ class SimpleAdapterActivity : AppCompatActivity() {
             intArrayOf(android.R.id.text1, android.R.id.text2)
         )
         listView.adapter = adapter
+
+        val intent = Intent(this,ListView_Add_DeleteActivity::class.java)
+        binding.btnSimpleToAdd.setOnClickListener { startActivity(intent) }
+
     }
 }
